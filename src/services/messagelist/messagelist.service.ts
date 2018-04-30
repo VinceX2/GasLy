549f0d4c4b7b8389f2107e5c760323ecd0ddc2ed
VinceX2 <vince.geek404@gmail.com>
2018-04-30 13:19:31 -0700

    Agregando package-lock.json a ,gitignore

--- src/services/messagelist/messagelist.service.ts
+++ src/services/messagelist/messagelist.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Send } from './../../models/message/send.model';
 @Injectable()
+
 export class SendMessageService {
   private sendMessageRef = this.db.list<Send>('messagelist');
   constructor(private db: AngularFireDatabase) {}
